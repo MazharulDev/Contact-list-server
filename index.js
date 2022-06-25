c281ed77960ddf1390e6dec0f5e8348504004d29
Mazharul islam <mazharulislam.dev@gmail.com>
2022-06-25 08:49:46 +0600

    get and post api created

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,33 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.npmlioa.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const contactCollection = client.db('contactList').collection('allContact')
+        //add contact
+        app.post('/allContact', async (req, res) => {
+            const newContact = req.body;
+            const result = await contactCollection.insertOne(newContact);
+            res.send(result);
+        })
+        app.get('/allContact', async (req, res) => {
+            const contacts = await contactCollection.find().toArray();
+            res.send(contacts);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('running test')
